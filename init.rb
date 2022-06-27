011c997a3e060a4b0efe5eb14d02e384d7aeb48e
Mwadime <brian.mwadime@gmail.com>
2022-06-27 18:05:56 +0300

    Update require statements

--- init.rb
+++ init.rb
@@ -1,21 +1,21 @@
 require 'redmine'
 
-require 'event_notification/patches/users_helper_patch'
-require 'event_notification/patches/user_patch'
-require 'event_notification/patches/project_patch'
-require 'event_notification/patches/member_patch'
-require 'event_notification/patches/issue_patch'
-require 'event_notification/patches/document_patch'
-require 'event_notification/patches/journal_patch'
-require 'event_notification/patches/message_patch'
-require 'event_notification/patches/wiki_content_patch'
-require 'event_notification/patches/watchers_controller_patch'
-require 'event_notification/patches/groups_controller_patch'
-require 'event_notification/patches/principal_memberships_controller_patch'
-require 'event_notification/patches/user_preference_patch'
-require 'event_notification/patches/custom_field_patch'
-require 'event_notification/patches/news_patch'
-require 'event_notification/patches/watcher_patch'
+require_relative './lib/event_notification/patches/users_helper_patch'
+require_relative './lib/event_notification/patches/user_patch'
+require_relative './lib/event_notification/patches/project_patch'
+require_relative './lib/event_notification/patches/member_patch'
+require_relative './lib/event_notification/patches/issue_patch'
+require_relative './lib/event_notification/patches/document_patch'
+require_relative './lib/event_notification/patches/journal_patch'
+require_relative './lib/event_notification/patches/message_patch'
+require_relative './lib/event_notification/patches/wiki_content_patch'
+require_relative './lib/event_notification/patches/watchers_controller_patch'
+require_relative './lib/event_notification/patches/groups_controller_patch'
+require_relative './lib/event_notification/patches/principal_memberships_controller_patch'
+require_relative './lib/event_notification/patches/user_preference_patch'
+require_relative './lib/event_notification/patches/custom_field_patch'
+require_relative './lib/event_notification/patches/news_patch'
+require_relative './lib/event_notification/patches/watcher_patch'
 
 require 'event_notification/patches/mailer_patch'
 
@@ -24,7 +24,7 @@ ActionDispatch::Callbacks.to_prepare do
 end
 
 Rails.configuration.to_prepare do
-  require 'event_notification/patches/acts_as_watchable_patch'
+  require_relative './lib/event_notification/patches/acts_as_watchable_patch'
 end
 
 Redmine::Plugin.register :event_notifications do
